004b31fcc826afe6f1d24714d21abe014f1a78b5
jose55mase <josemase55@gmail.com>
2019-07-24 17:27:21 -0500

    product

--- app/services/Sproducts.py
+++ app/services/Sproducts.py
@@ -6,44 +6,43 @@ import json
 #from flask_sqlalchemy import SQLAlchemy
 
 #   Operaciones SQL  con categorias
-class sProducts:
+class Sproducts:
     #   constructor
-    def __init__(
-                    self
-                    ,Categoria_id=None
-                    ,Titulo=None
-                    ,Descripcion=None
-                    ,Precio = None
-                    ,Precio_Descuento = None
-                    ,Stock_Actual = None
-                    ,Stock_Limite = None
-                    ,Estado = None
-                    ,Referencia_Amazon = None
-                    ,Peso = None
-                    ,Alto = None
-                    ,Largo = None
-                    ,Ancho = None
-                    ,Color = None
-                    ,Talla = None
-                    ,Porcentaje = None
-                    ,Precio_cop = None
-                    ,Creado_por = None
-                    ,Modicado_por = None
-                    ,Fecha_Creacion = None
-                    ,Fecha_Actualizacion = None
-                    ,Codigo = None
-                    ,breadcrum = None
-                    ,GUID = None
-                    ,SKU = None
-                    ,informacion = None
-                    ,Imagenes_1 = None
-                    ,Imagenes_2 = None
-                    ,Imagenes_3 = None
-                    ,Imagenes_4 = None
-                    ,Imagenes_5 = None
-                    ,Imagenes_6 = None
-                    ,Imagenes_7 = None
-                ):
+    def __init__(self
+            ,Categoria_id=None
+            ,Titulo=None
+            ,Descripcion=None
+            ,Precio = None
+            ,Precio_Descuento = None
+            ,Stock_Actual = None
+            ,Stock_Limite = None
+            ,Estado = None
+            ,Referencia_Amazon = None
+            ,Peso = None
+            ,Alto = None
+            ,Largo = None
+            ,Ancho = None
+            ,Color = None
+            ,Talla = None
+            ,Porcentaje = None
+            ,Precio_cop = None
+            ,Creado_Por = None
+            ,Modificado_Por = None
+            ,Fecha_Creacion = None
+            ,Fecha_Actualizacion = None
+            ,Codigo = None
+            ,breadcrum = None
+            ,GUID = None
+            ,SKU = None
+            ,informacion = None
+            ,Imagenes_1 = None
+            ,Imagenes_2 = None
+            ,Imagenes_3 = None
+            ,Imagenes_4 = None
+            ,Imagenes_5 = None
+            ,Imagenes_6 = None
+            ,Imagenes_7 = None
+        ):
         """ Campos """
         self.__Categoria_id = Categoria_id
         self.__Titulo = Titulo
@@ -62,8 +61,8 @@ class sProducts:
         self.__Talla = Talla
         self.__Porcentaje = Porcentaje
         self.__Precio_cop = Precio_cop
-        self.__Creado_por = Creado_por
-        self.__Modicado_por = Modicado_por
+        self.__Creado_Por = Creado_Por
+        self.__Modificado_Por = Modificado_Por
         self.__Fecha_Creacion = Fecha_Creacion
         self.__Fecha_Actualizacion = Fecha_Actualizacion
         self.__Codigo = Codigo
@@ -71,15 +70,14 @@ class sProducts:
         self.__GUID = GUID
         self.__SKU = SKU
         self.__informacion = informacion
-        self.__Imagenes_1 = Imagenes_1
-        self.__Imagenes_2 = Imagenes_2
-        self.__Imagenes_3 = Imagenes_3
-        self.__Imagenes_4 = Imagenes_4
-        self.__Imagenes_5 = Imagenes_5
-        self.__Imagenes_6 = Imagenes_6
-        self.__Imagenes_7 = Imagenes_7
+        self.__Imagenes_1 = Imagenes_1 if Imagenes_1 is not None else 'Sin imagen'
+        self.__Imagenes_2 = Imagenes_2 if Imagenes_2 is not None else 'Sin imagen'
+        self.__Imagenes_3 = Imagenes_3 if Imagenes_3 is not None else 'Sin imagen'
+        self.__Imagenes_4 = Imagenes_4 if Imagenes_4 is not None else 'Sin imagen'
+        self.__Imagenes_5 = Imagenes_5 if Imagenes_5 is not None else 'Sin imagen'
+        self.__Imagenes_6 = Imagenes_6 if Imagenes_6 is not None else 'Sin imagen'
+        self.__Imagenes_7 = Imagenes_7 if Imagenes_7 is not None else 'Sin imagen'
         self.__constring = return_conection_string(argument='mssql', db_database='DBKiero_Productos')
-        #self.__constring = connectionString().connectionODBC() #    Conexion a la base de datos.
         pass
 
     #   Trae la lista de  productos
@@ -91,7 +89,7 @@ class sProducts:
             sentencia = "SELECT * FROM tbl_Productos_pruebas_mk"
             cursor.execute(sentencia)
             resultado = cursor.fetchall()
-        #   Se recorre los resultados y se guardan el un array
+        #   Se recorre los resultados y se guardan en un array
         array = [] #    El array de retorno
         json = {        }
         for row in resultado:
@@ -106,17 +104,10 @@ class sProducts:
         cursor = conexion.cursor()
         """ Insert """
         cursor.execute("""
-        INSERT INTO tbl_Productos_pruebas_mk
-                (
-                    Categoria_id ,Titulo, Descripcion, Precio, Precio_Descuento, Stock_Actual, Stock_Limite
-                    ,Estado ,Referencia_Amazon, Peso,Alto,Largo,Ancho,Color,Talla,Porcentaje,Precio_cop,Creado_por,Modicado_por,Fecha_Creacion,Fecha_Actualizacion,Codigo,breadcrum,GUID,SKU,informacion,imagenes_1,imagenes_2,imagenes_3,imagenes_4,imagenes_5,imagenes_6,imagenes_7
+        INSERT INTO tbl_Productos_pruebas_mk VALUES (
+                    '{1}', '{2}', '{3}','{4}','{5}','{6}','{7}','{8}','{}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}','{2}',
                 )
-        VALUES
-        (
-            '{Categoria_id}','{Titulo}','{Descripcion}','{Precio}','{Precio_Descuento}','{Stock_Actual}','{Stock_Limite}','{Estado}','{Referencia_Amazon}','{Peso}','{Alto}','{Largo}','{Ancho}','{Color}','{Talla}','{Porcentaje}','{Precio_cop}','{Creado_por}','{Modicado_por}','{Fecha_Creacion}','{Fecha_Actualizacion}','{Codigo}','{breadcrum}','{GUID}','{SKU}','{informacion}','{Imagenes_1}','{Imagenes_2}','{Imagenes_3}','{Imagenes_4}','{Imagenes_5}','{Imagenes_6}','{Imagenes_7}'
-
-        )
-        """.format(
+                 """.format(
                     Categoria_id=self.__Categoria_id
                     ,Titulo=self.__Titulo
                     ,Descripcion=self.__Descripcion
@@ -134,8 +125,8 @@ class sProducts:
                     ,Talla=self.__Talla
                     ,Porcentaje = self.__Porcentaje
                     ,Precio_cop=self.__Precio_cop
-                    ,Creado_por=self.__Creado_por
-                    ,Modicado_por=self.__Modicado_por
+                    ,Creado_Por=self.__Creado_Por
+                    ,Modificado_Por=self.__Modificado_Por
                     ,Fecha_Creacion=self.__Fecha_Creacion
                     ,Fecha_Actualizacion =self.__Fecha_Actualizacion
                     ,Codigo=self.__Codigo
--- routes/add_route/new.py
+++ routes/add_route/new.py
@@ -8,7 +8,7 @@
 from flask import jsonify, request
 from routes.router import app,resource,response,req,reqpar
 
-from app.services.sproducts import sProducts
+from app.services.Sproducts import Sproducts
 
 import json
 
@@ -25,9 +25,8 @@ class new(resource):
         
     def get(self):             
         objectoJson = {'Mensaje':None,'Resultados':[]}      
-        try:   
-            print("Process Load")         
-            retorno = sProducts().GET_ALL_PRODUCTS()
+        try:            
+            retorno = Sproducts().GET_ALL_PRODUCTS()
             listaJson = json.dumps(retorno)
             jsonTemplate = response(listaJson,status=200, mimetype='application/json')
             jsonTemplate.headers['Access-Control-Allow-Origin'] = '*'
@@ -37,30 +36,33 @@ class new(resource):
             print(exp)
             return {'Mensaje':'Problema interno'},500 
     
-#    def post(self):
- #       data = request.get_json()
-#     print(data)        
- #       return data
 
     def post(self):
-        methdPost = request.get_json()
         try:
-            print("Insert Load", methdPost["Creado_Por"])
+            methdPost = request.get_json()
+            Categoria_id=methdPost["Categoria_id"]
+            Titulo=methdPost["Titulo"]
+            Descripcion=methdPost["Descripcion"]
+            Estado=methdPost["Estado"]
+            Referencia_Amazon=methdPost["Referencia_Amazon"]
+            Creado_Por=methdPost["Creado_Por"]
+            Modificado_Por=methdPost["Modificado_Por"]
+            Fecha_Creacion=methdPost["Fecha_Creacion"]
+            Fecha_Actualizacion=methdPost["Fecha_Actualizacion"]
 
-            data = sProducts(
-                Categoria_id=methdPost["Categoria_id"]
-                ,Titulo=methdPost["Titulo"]
-                ,Descripcion=methdPost["Descripcion"]
-                ,Estado=methdPost["Estado"]
-                ,Referencia_Amazon=methdPost["Referencia_Amazon"]
-                ,Creado_Por=methdPost["Creado_Por"]
-                ,Modificado_Por=methdPost["Modificado_Por"]
-                ,Fecha_Creacion=methdPost["Fecha_Creacion"]
-                ,Fecha_Actualizacion=methdPost["Fecha_Actualizacion"]
+            Sproducts(
+                Categoria_id= Categoria_id
+                ,Titulo=Titulo
+                ,Descripcion=Descripcion
+                ,Estado=Estado
+                ,Referencia_Amazon=Referencia_Amazon
+                ,Creado_Por=Creado_Por
+                ,Modificado_Por=Modificado_Por
+                ,Fecha_Creacion=Fecha_Creacion
+                ,Fecha_Actualizacion=Fecha_Actualizacion
                 ).INSERT_PRODUCT()
             print("Insert call")
-            print(data)        
-            return data
+            
         except Exception as error:
             print(error)
             return {'Mensaje':'Problema interno'},500
